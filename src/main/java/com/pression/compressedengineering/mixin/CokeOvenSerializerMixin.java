578fa34fa8031dbf7bda0d83770d187478206df2
Big Barza <francesco.b99@hotmail.it>
2024-06-28 21:16:43 +0200

    RecipeGetterMixin rework (and test recipe) - Reworked RecipeGetterMixin to use @Inject instead of @Overwrite. One down, 3 to go. - Tweaked the serializer to just create the FluidStack for creosote as well. There's no sugarcoating it, the previous implementation was just dumb. - Readded the testing recipe. Hopefully i remember to remove it.

--- src/main/java/com/pression/compressedengineering/mixin/CokeOvenSerializerMixin.java
+++ src/main/java/com/pression/compressedengineering/mixin/CokeOvenSerializerMixin.java
@@ -5,6 +5,7 @@ import blusunrize.immersiveengineering.api.crafting.CokeOvenRecipe;
 import blusunrize.immersiveengineering.api.crafting.IERecipeSerializer;
 import blusunrize.immersiveengineering.api.crafting.IngredientWithSize;
 import blusunrize.immersiveengineering.common.crafting.serializers.CokeOvenRecipeSerializer;
+import blusunrize.immersiveengineering.common.register.IEFluids;
 import com.pression.compressedengineering.interfaces.CokeOvenFluidOutput;
 import com.google.gson.JsonObject;
 import net.minecraftforge.fluids.FluidStack;
@@ -31,10 +32,13 @@ abstract public class CokeOvenSerializerMixin extends IERecipeSerializer<CokeOve
         int time = GsonHelper.getAsInt(json, "time");
         int oil = 0; //If there's no creosote nor other fluid, this must be 0 to ensure fluids don't get drawn in jei.
         FluidStack fluidOutput = FluidStack.EMPTY;
-        if(json.has("creosote")) oil = GsonHelper.getAsInt(json, "creosote");
+        if(json.has("creosote")){
+            oil = GsonHelper.getAsInt(json, "creosote");
+            fluidOutput = new FluidStack(IEFluids.CREOSOTE.getStill(), oil);
+        }
         if(json.has("fluid")){
             fluidOutput = ApiUtils.jsonDeserializeFluidStack(GsonHelper.getAsJsonObject(json, "fluid"));
-            oil = 10; //Dummy value to ensure fluids get drawn anyway
+            oil = 1; //Dummy value to ensure fluids get drawn anyway
         }
         CokeOvenRecipe recipe = new CokeOvenRecipe(recipeId, output, input, time, oil);
         CokeOvenFluidOutput fluidAdder = (CokeOvenFluidOutput) recipe;
--- src/main/java/com/pression/compressedengineering/mixin/RecipeGetterMixin.java
+++ src/main/java/com/pression/compressedengineering/mixin/RecipeGetterMixin.java
@@ -3,29 +3,39 @@ package com.pression.compressedengineering.mixin;
 import blusunrize.immersiveengineering.api.crafting.CokeOvenRecipe;
 import blusunrize.immersiveengineering.common.blocks.stone.CokeOvenBlockEntity;
 import com.pression.compressedengineering.interfaces.CokeOvenFluidOutput;
-import net.minecraft.core.NonNullList;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import javax.annotation.Nullable;
-import java.util.function.Supplier;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(CokeOvenBlockEntity.class)
 public abstract class RecipeGetterMixin {
-    @Final @Shadow(remap = false) private Supplier<CokeOvenRecipe> cachedRecipe;
+    @Shadow(remap = false) public FluidTank tank;
+    @Inject(remap = false, method = "getRecipe()Lblusunrize/immersiveengineering/api/crafting/CokeOvenRecipe;", cancellable = true, at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void fluidRecipeCheck(CallbackInfoReturnable<CokeOvenRecipe> cir, CokeOvenRecipe recipe){
+        if(cir.getReturnValue() == null) return; //We want to leave the other returns alone.
+        CokeOvenFluidOutput recipeFluid = (CokeOvenFluidOutput) recipe;
+        if(recipe.creosoteOutput == 0) return; //No fluid output, nothing to handle.
+        if(tank.isEmpty() || tank.getFluid().isFluidEqual(recipeFluid.getFluidOutput())){ //If the tank is empty or the fluid matches
+            if(tank.getFluidAmount() + recipeFluid.getFluidOutput().getAmount() <= tank.getCapacity()) return; //And there's enough space, let the recipe trough.
+        }
+        cir.setReturnValue(null); //If we got here, the check has failed and no recipe should be actually returned.
+    }
+}
+
+//Previous mixin, this is here as a backup
+
+/*
+    @Final
+    @Shadow(remap = false) private Supplier<CokeOvenRecipe> cachedRecipe;
     @Final @Shadow(remap = false) private NonNullList<ItemStack> inventory;
     @Final @Shadow(remap = false) public static int OUTPUT_SLOT;
-    @Shadow(remap = false) public FluidTank tank;
     @Shadow(remap = false) public abstract int getSlotLimit(int slot);
 
-    /**
-     * @author Big Barza
-     * @reason Intellij made me do this.
-     */
+
     @Nullable
     @Overwrite(remap = false)
     public CokeOvenRecipe getRecipe()
@@ -41,4 +51,5 @@ public abstract class RecipeGetterMixin {
                 return recipe;
         return null;
     }
-}
+    */
+
