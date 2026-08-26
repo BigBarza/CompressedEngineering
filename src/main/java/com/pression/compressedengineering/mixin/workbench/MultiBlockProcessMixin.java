9e826f9c8b385e34770c97b496d4d29550975e1f
Big Barza <francesco.b99@hotmail.it>
2026-08-26 19:54:42 +0200

    Fixed crushers being broken by the patch for oversized blueprint recipes.

--- src/main/java/com/pression/compressedengineering/mixin/workbench/MultiBlockProcessMixin.java
+++ src/main/java/com/pression/compressedengineering/mixin/workbench/MultiBlockProcessMixin.java
@@ -1,6 +1,10 @@
 package com.pression.compressedengineering.mixin.workbench;
 
+import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
+import blusunrize.immersiveengineering.api.crafting.MultiblockRecipe;
+import blusunrize.immersiveengineering.api.multiblocks.blocks.env.IMultiblockLevel;
 import blusunrize.immersiveengineering.common.blocks.multiblocks.process.MultiblockProcessInWorld;
+import blusunrize.immersiveengineering.common.blocks.multiblocks.process.ProcessContext;
 import com.pression.compressedengineering.CommonConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,9 +15,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MultiBlockProcessMixin {
     @Inject(method = "processFinish(Lblusunrize/immersiveengineering/common/blocks/multiblocks/process/ProcessContext$ProcessContextInWorld;Lblusunrize/immersiveengineering/api/multiblocks/blocks/env/IMultiblockLevel;)V"
     , at = @At(value = "INVOKE", target = "Lblusunrize/immersiveengineering/api/multiblocks/blocks/env/IMultiblockLevel;getRawLevel()Lnet/minecraft/world/level/Level;"), remap = false, cancellable = true)
-    private void yeetPointlessCheck(CallbackInfo ci){
+    private <R extends MultiblockRecipe> void yeetPointlessCheck(ProcessContext.ProcessContextInWorld<R> context, IMultiblockLevel level, CallbackInfo ci){
         //The check in processFinish tries to find "oversized" stacks, and restarts the process if it finds any. Note that the inputs were already validated.
         //This creates more of the output than intended. So off it goes.
-        if(CommonConfig.ALT_BP_RECIPE_COUNT_CHECK.get()) ci.cancel();
+        MultiblockRecipe recipe = ((MultiblockProcessInWorld<?>)(Object)this).getRecipe(level.getRawLevel());
+        if(recipe == null) return; // The recipe check is needec cause apparently it can mess up other multis like crushers.
+        if(CommonConfig.ALT_BP_RECIPE_COUNT_CHECK.get() && recipe instanceof BlueprintCraftingRecipe bp) ci.cancel();
     }
 }
